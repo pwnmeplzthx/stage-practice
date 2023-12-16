df2c928536ebf013230f12c573ddecb300b81017
pwnmeplzthx <pshumakov11@mail.ru>
2023-12-16 19:53:15 +0300

    database init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import { Button } from "@/shared/ui/button";
+import { PrismaClient } from "@prisma/client";
+import { useEffect } from "react";
+
+const client = new PrismaClient();
 
 export default function Home() {
+    const productsHandler = async () => {
+        const products = await client.product.findMany();
+        console.log(products)
+    }
+    
+    productsHandler();
+    
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <Button>Button</Button>
