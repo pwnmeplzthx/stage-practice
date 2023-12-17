cf233f3c610e312e0703f1f88a179f7dd4ad66d7
pwnmeplzthx <pshumakov11@mail.ru>
2023-12-17 13:42:55 +0300

    another test ci/cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col p-8">
             <h1>Testing ci/cd</h1>
+            <h2>another test ci/cd</h2>
             <CreateProductForm className="max-w-[300px] mb-10" revalidatePagePath="/" />
             <ProductsList revalidatePagePath="/" />
         </main>
